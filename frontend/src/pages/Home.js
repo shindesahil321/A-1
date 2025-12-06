f54a08a23f586360a3999f8d0b59f398c9aa46cc
Sahilshinde123 <sahilshinde3377@gmail.com>
2025-12-07 01:11:20 +0530

    fix(ci): remove unused default React imports to satisfy ESLint in CI

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { handleError, handleSuccess } from '../utils';
 import { ToastContainer } from 'react-toastify';
